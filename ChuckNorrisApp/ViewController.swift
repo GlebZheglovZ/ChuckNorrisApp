abc08f7e4e0490446c75641a6f04a704596e579c
Gleb Nikolaev <marvelheroes92@gmail.com>
2019-11-16 12:00:52 +0300

    Initial Commit

---
+++ ChuckNorrisApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorrisApp
+//
+//  Created by Глеб Николаев on 16.11.2019.
+//  Copyright © 2019 Глеб Николаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
